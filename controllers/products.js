e71533329dbf0106d29bae3d2e69ae1e843f3350
Tanish <tanishthorat02@gmail.com>
2023-06-24 20:16:36 +0530

    MVC  commit

---
+++ controllers/products.js
@@ -0,0 +1,32 @@
+
+
+const Product = require('../models/product');
+
+exports.getaddproducts = (req, res, next) => {
+    res.render('add-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true
+    });
+  };
+
+exports.postaddproducts = (req, res, next) => {
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/');
+  }
+
+  exports.getproducts = (req, res, next) => {
+    // const products = adminData.products;
+     const products = Product.fetchAllProducts();
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  }
---
+++ views/pract.js
@@ -0,0 +1,7 @@
+const arr = ['tanish', 'thorat'];
+const obg = { name:'tanish',
+sirname: 'thorat'};
+
+const one = {...obg};
+console.log(one);
+
